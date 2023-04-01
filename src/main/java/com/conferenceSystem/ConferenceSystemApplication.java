677fd034170df315815c3b66d1b4793c381f607d
lata2016 <qosjadhurata@gmail.com>
2023-04-01 14:58:03 +0200

    Struktura e projektit

---
+++ src/main/java/com/conferenceSystem/ConferenceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.conferenceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/conferenceSystem/ConferenceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conferenceSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
