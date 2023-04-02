6d24c1a3411ab0dd7affd03e0d2bd31c05237fa5
xhemal <xhemal.llalla@outlook.com>
2023-04-02 09:54:44 +0200

    test2

--- src/main/java/com/conferenceSystem/ConferenceSystemApplication.java
+++ src/main/java/com/conferenceSystem/ConferenceSystemApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConferenceSystemApplication {
 
-
-
-
-
 	
 
 	public static void main(String[] args) {
